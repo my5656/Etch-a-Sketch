f8f81c1563c7f8868f9d2f6c4ec8114f91cc580a
Megumi Yoda <>
2024-09-07 16:53:16 -0600

    Create a feature for a user to enter desired number of grids to recreate the entire grid in a desired size.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -11,3 +11,27 @@ container.addEventListener('mouseover', (event)=>{
         event.target.style.backgroundColor = '#87ceeb';
     }
 }); 
+
+const numberInput = document.getElementById("numberInput");
+const confirmButton = document.getElementById("confirmButton");
+confirmButton.addEventListener("click", function(){
+    const gridNumber = parseInt(numberInput.value);
+    if (isNaN(gridNumber) || gridNumber<=0){
+        alert("Please enter a valid number greater than 0.");
+    }
+    if (gridNumber > 100){
+        alert("Please enter a number less than 100.");
+    }
+    recreateGrid(gridNumber);
+});
+function recreateGrid(gridNumber){
+    container.innerHTML = '';
+    const gridSize = 800/gridNumber;
+    for (let i=0; i < gridNumber*gridNumber; i++){
+        const newDiv = document.createElement('div');
+        newDiv.classList.add('div-class');
+        newDiv.style.width = `${gridSize}px`;
+        newDiv.style.height = `${gridSize}px`;
+        container.appendChild(newDiv);
+    }
+};
