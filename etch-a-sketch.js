3e85a0a7e1cbf8ce57f1d87392aac9e4070b2e78
Megumi Yoda <>
2024-09-09 10:49:00 -0600

    Added transition of opacity on grids when mouse is hovered on them.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -3,17 +3,22 @@ let gridSize = 16;
 createInitialGrid(gridSize);
 
 function createInitialGrid(gridSize) {
-    container.style.setProperty('--grid-size', gridSize);
+    container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+    /*container.style.setProperty('--grid-size', gridSize);*/
     for (let i = 0; i < gridSize * gridSize; i++) {
         const newDiv = document.createElement('div');
-        newDiv.classList.add('div-class');
+        newDiv.classList.add('grid-item');
         container.appendChild(newDiv);
     }
 };
-
 container.addEventListener('mouseover', (event) => {
-    if (event.target.classList.contains('div-class')) {
-        event.target.style.backgroundColor = '#87ceeb';
+    if (event.target.classList.contains('grid-item')) {
+        let currentOpacity = parseFloat(getComputedStyle(event.target).backgroundColor.split(',')[3]);
+        if (currentOpacity < 1) {
+            currentOpacity = Math.min(currentOpacity + 0.1, 1);
+            event.target.style.backgroundColor = `rgba(135, 206, 235, ${currentOpacity})`;
+        }
     }
 });
 
